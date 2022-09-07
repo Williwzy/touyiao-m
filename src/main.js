f51579b7008080f7e192c015e505a976000f5a1d
Williwzy <2410003596@qq.com>
2022-09-07 11:25:45 +0800

    项目初始化引入vant组件

--- src/main.js
+++ src/main.js
@@ -2,10 +2,14 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import Vant from 'vant'
+// 加载vant全局样式
+import 'vant/lib/index.css'
 
 import './styles/index.less'
 Vue.config.productionTip = false
-
+// 注册使用vant组件
+Vue.use(Vant)
 new Vue({
   router,
   store,
